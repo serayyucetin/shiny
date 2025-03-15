0b5e02eeedcb802df3b2a6a31245d42849309f93
serayyucetin <serayyucetinnn@gmail.com>
2025-03-15 19:54:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,41 +28,46 @@ from zipfile import ZipFile
 
 from shiny.types import FileInfo
 import umapcodes #codes needed for umap visualization importet as module
-#user interface 
+
 app_ui = ui.page_fluid(
     ui.input_file("file1", "Choose zip File", accept=[".zip"], multiple=False),
-    ui.input_checkbox_group(
-        "stats",
-        "Summary Stats",
-        choices=["Row Count", "Column Count", "Column Names"],
-        selected=["Row Count", "Column Count", "Column Names"],
-    ),
     ui.output_table("summary"),
     ui.h1("Single-Cell Data UMAPViewer in Shiny for Python"),
-    ui.output_image("final_plot")
+    ui.output_plot("umap_plot")
 )
 
 
 def server(input: Inputs, output: Outputs, session: Session):
     @output
-    def unzip_file(zip_path, extract_to):
-        with zipfile.ZipFile(zip_path,'r') as zip_ref:
-            zip_ref.extractall(extract_to)    
-    @output
-    @reactive.calc
-    def parsed_file():
-        file: list[FileInfo] | None = input.file1()
-        try:
-            if file is None:
-                return pd.DataFrame()
-                return pd.read_h5ad( 
-                      file[0]["datapath"] )
-        except:
-            return f"file uploaded"
-    @output
-    @render.table
-    def summary():
-        df = parsed_file()
+    @render.plot
+    @reactive.event(input.file1) 
+    def umap_plot():
+        file = input.file1()  
+        if file is not None:  # Dosya yüklendiyse
+            if isinstance(file, list):  # Eğer file bir liste ise
+                file_path = file[0]['datapath']  # İlk dosyanın yolunu al
+            else:
+                file_path = file[0]["data_path"]
+            
+            timestamp = int(time.time())  # Get current time in seconds
+            extracted_folder = f"extracted_data_{timestamp}"
+            unzip_file(file_path, extracted_folder)
+            extracted_files = os.listdir(extracted_folder)
+            print(f"Files in extracted folder: {extracted_files}")
+            h5ad_files = [f for f in os.listdir(extracted_folder) if f.endswith('.h5ad')]
+            
+            if not h5ad_files:
+                print("No .h5ad file found in the extracted folder")
+                return
+            
+            
+        adata_file = h5ad_files[0]
+        adata = sc.read(os.path.join(extracted_folder, adata_file))
+        adata = filter(adata) 
+        cluster(adata)
+        return plot(adata)
 
-        if df.empty:
-            return pd.DataFrame()
+
+            
+App(app_ui, server) 
+app = App(app_ui,server)
